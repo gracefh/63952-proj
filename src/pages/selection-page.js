78a0286ea00158bd8c5ae95f92b493c0d8e03fb8
Catherine Mei <meic1212@mit.edu>
2023-12-05 23:40:26 -0500

    filters and sorting

--- src/pages/selection-page.js
+++ src/pages/selection-page.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import Grid from "@mui/material/Grid";
@@ -9,22 +9,58 @@ import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Navbar from "../components/navbar";
 import ArtCard from "../components/art-card";
+import Autocomplete, { createFilterOptions } from "@mui/material/Autocomplete";
+import Chip from "@mui/material/Chip";
+import TextField from "@mui/material/TextField";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Collapse,
+} from "@mui/material";
 
 const defaultTheme = createTheme();
+
+const artisticStyles = [
+  "Abstract",
+  "Baroque",
+  "Surrealism",
+  "Impressionism",
+  "Cubism",
+  "Rococo",
+  "Realism",
+  "Expressionism",
+  "Minimalism",
+  "Renaissance",
+  "Pop Art",
+  "Futurism",
+  "Art Nouveau",
+  "Gothic",
+  "Neo-Classicism",
+  "Romanticism",
+  "Graffiti",
+  "Digital",
+  "Photorealism",
+  "Conceptual",
+  "Modernism",
+  "Other",
+];
+
 const dummyData = [
   {
     id: 1,
     name: "Image1.jpg",
     price: 1,
     tags: ["Expressionism"],
-    artist: "Charlie",
+    artist: "Catherine",
   },
   {
     id: 2,
     name: "Image2.jpg",
     price: 12,
     tags: ["Rococo"],
-    artist: "Casey",
+    artist: "Grace",
   },
   {
     id: 3,
@@ -65,7 +101,7 @@ const dummyData = [
     id: 8,
     name: "Image8.jpg",
     price: 0,
-    tags: ["Surrealism"],
+    tags: ["Surrealism", "My Label"],
     artist: "Ivy",
   },
   {
@@ -77,20 +113,75 @@ const dummyData = [
   },
 ];
 
+const maxPrice = 100;
+
+const filterOptions = createFilterOptions({
+  matchFrom: "start",
+  stringify: (option) => option,
+});
+
 export default function SelectionPage() {
   const [images, setImages] = useState(dummyData);
+  const [selectedStyles, setSelectedStyles] = useState([]);
+  const [priceRange, setPriceRange] = useState([0, maxPrice]);
+  const [sortOrder, setSortOrder] = useState("");
+  const [showStyleFilter, setShowStyleFilter] = useState(false);
+  const [showPriceFilter, setShowPriceFilter] = useState(false);
+
+  const handleStyleChange = (event, value) => {
+    setSelectedStyles(value);
+  };
+
+  const handlePriceChange = (event, newValue) => {
+    setPriceRange(newValue);
+  };
+
+  const handleSortChange = (event) => {
+    setSortOrder(event.target.value);
+  };
+
+  const clearAllFilters = () => {
+    setSelectedStyles([]);
+    setPriceRange([0, maxPrice]);
+    setSortOrder("");
+  };
+
+  const filteredImages = useMemo(() => {
+    return images
+      .filter((image) => {
+        if (selectedStyles.includes("Other")) {
+          return image.tags.some(
+            (tag) =>
+              !artisticStyles.includes(tag) || selectedStyles.includes(tag)
+          );
+        }
+        return (
+          selectedStyles.length === 0 ||
+          image.tags.some((tag) => selectedStyles.includes(tag))
+        );
+      })
+      .filter(
+        (image) => image.price >= priceRange[0] && image.price <= priceRange[1]
+      )
+      .sort((a, b) => {
+        if (sortOrder === "low-high") {
+          return a.price - b.price;
+        } else if (sortOrder === "high-low") {
+          return b.price - a.price;
+        }
+        return 0;
+      });
+  }, [images, selectedStyles, priceRange, sortOrder]);
 
   return (
     <ThemeProvider theme={defaultTheme}>
       <CssBaseline />
       <Navbar />
       <main>
-        {/* Hero unit */}
         <Box
           sx={{
             bgcolor: "background.paper",
-            pt: 8,
-            pb: 6,
+            pt: 6,
           }}
         >
           <Container maxWidth="sm">
@@ -123,14 +214,126 @@ export default function SelectionPage() {
           </Container>
         </Box>
         <Container sx={{ py: 8 }} maxWidth="md">
-          {/* Grid for Art Cards */}
-          <Grid container spacing={4}>
-            {images.map((card) => (
-              <Grid item key={card.id} xs={12} sm={6} md={4}>
-                <ArtCard card={card} />
-              </Grid>
-            ))}
-          </Grid>
+          <Box
+            sx={{
+              position: "relative",
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <Box>
+              <Button onClick={() => setShowStyleFilter(!showStyleFilter)}>
+                Art Style Filter
+              </Button>
+              <Collapse
+                in={showStyleFilter}
+                sx={{ position: "absolute", zIndex: 1, minWidth: 150, mt: 1 }}
+              >
+                <Autocomplete
+                  multiple
+                  options={artisticStyles}
+                  value={selectedStyles}
+                  onChange={handleStyleChange}
+                  filterOptions={filterOptions}
+                  freeSolo
+                  renderTags={(value, getTagProps) =>
+                    value.map((option, index) => (
+                      <Chip
+                        variant="outlined"
+                        label={option}
+                        size="small"
+                        {...getTagProps({ index })}
+                      />
+                    ))
+                  }
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      label="Art Styles"
+                      placeholder="Select or type"
+                    />
+                  )}
+                />
+              </Collapse>
+            </Box>
+            {/* Spacer box */}
+            <Box flexGap={4} />
+            <Box>
+              <Button onClick={() => setShowPriceFilter(!showPriceFilter)}>
+                Price Filter
+              </Button>
+              <Collapse
+                in={showPriceFilter}
+                sx={{ position: "absolute", zIndex: 1, width: 300, mt: 1 }}
+              >
+                <Box>
+                  <Stack direction="row" spacing={2}>
+                    <TextField
+                      label="$"
+                      type="number"
+                      value={priceRange[0]}
+                      onChange={(e) =>
+                        setPriceRange([
+                          Math.max(0, Number(e.target.value)),
+                          priceRange[1],
+                        ])
+                      }
+                      inputProps={{ min: 0, max: priceRange[1] }}
+                      sx={{ width: "85px" }}
+                    />
+                    <TextField
+                      label="$$$"
+                      type="number"
+                      value={priceRange[1]}
+                      onChange={(e) =>
+                        setPriceRange([
+                          priceRange[0],
+                          Math.min(Number(e.target.value), maxPrice),
+                        ])
+                      }
+                      inputProps={{ min: priceRange[0], max: maxPrice }}
+                      sx={{ width: "85px" }}
+                    />
+                  </Stack>
+                </Box>
+              </Collapse>
+            </Box>
+            {/* Spacer box */}
+            <Box flexGap={16} />
+            <Button
+              onClick={clearAllFilters}
+              variant="outlined"
+              sx={{
+                alignSelf: "center",
+              }}
+            >
+              Clear All Filters
+            </Button>
+
+            <FormControl sx={{ m: 1, minWidth: 120 }}>
+              <InputLabel>Sort By</InputLabel>
+              <Select
+                value={sortOrder}
+                label="Sort By"
+                onChange={handleSortChange}
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value="low-high">Price: Low to High</MenuItem>
+                <MenuItem value="high-low">Price: High to Low</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+          <Box sx={{ marginTop: 5 }}>
+            <Grid container spacing={4}>
+              {filteredImages.map((card) => (
+                <Grid item key={card.id} xs={12} sm={6} md={4}>
+                  <ArtCard card={card} />
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
         </Container>
       </main>
     </ThemeProvider>
