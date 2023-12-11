9f09c173702d98b14f7873397067c69ba4092723
Catherine Mei <meic1212@mit.edu>
2023-12-11 18:17:35 -0500

    removed unnecessary import

--- src/pages/shopping-cart.js
+++ src/pages/shopping-cart.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
