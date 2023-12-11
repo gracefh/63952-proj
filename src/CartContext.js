adbf8a0622c769421da3f68d42b05c1ca9151bcd
Catherine Mei <meic1212@mit.edu>
2023-12-11 00:29:53 -0500

    useEffect in cart context to load default values

--- src/CartContext.js
+++ src/CartContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext, useEffect } from "react";
 import axios from "axios";
 
 const CartContext = createContext();
@@ -7,6 +7,20 @@ export const useCart = () => useContext(CartContext);
 
 export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    const fetchCartItems = async () => {
+      try {
+        const response = await axios.get("/api/cart");
+        console.log("response:", response);
+        setCartItems(response.data.contents || []);
+      } catch (error) {
+        console.error("Error fetching cart items:", error);
+      }
+    };
+
+    fetchCartItems();
+  }, []);
 
   const addToCart = async (item) => {
     await axios.patch(`/api/cart/${item._id}`);
