4f5ed61f50a797aee34218b8879d8601236779a0
Catherine Mei <meic1212@mit.edu>
2023-12-11 00:24:22 -0500

    upload page allows entering tags + price

--- src/pages/art-upload.js
+++ src/pages/art-upload.js
@@ -3,21 +3,51 @@ import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import Input from "@mui/material/Input";
+import Paper from "@mui/material/Paper";
+import Alert from "@mui/material/Alert";
+import Navbar from "../components/navbar";
+import Chip from "@mui/material/Chip";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import axios from "axios";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
-import Input from "@mui/material/Input";
-import Paper from "@mui/material/Paper";
-import Alert from "@mui/material/Alert";
-import Navbar from "../components/navbar";
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { NumericFormat } from "react-number-format";
 
+const artisticStyles = [
+  "Abstract",
+  "Baroque",
+  "Surrealism",
+  "Impressionism",
+  "Cubism",
+  "Rococo",
+  "Realism",
+  "Expressionism",
+  "Minimalism",
+  "Renaissance",
+  "Pop Art",
+  "Futurism",
+  "Art Nouveau",
+  "Gothic",
+  "Neo-Classicism",
+  "Romanticism",
+  "Graffiti",
+  "Digital",
+  "Photorealism",
+  "Conceptual",
+  "Modernism",
+  "Other",
+];
 export default function ArtUploadPage() {
   const [selectedFile, setSelectedFile] = useState(undefined);
   const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [tags, setTags] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate();
 
@@ -40,6 +70,10 @@ export default function ArtUploadPage() {
     document.getElementById("file-input").value = "";
   };
 
+  const handlePriceChange = (event) => {
+    setPrice(event.target.value);
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     if (selectedFile === undefined) {
@@ -59,6 +93,8 @@ export default function ArtUploadPage() {
       await axios.post(`/api/art`, {
         title: title,
         link: `https://s3.us-east-2.amazonaws.com/6.3952-final-proj/${key}`,
+        price: price.substring(1),
+        tags: tags,
       });
       navigate("/your-art");
     } catch (error) {
@@ -151,10 +187,56 @@ export default function ArtUploadPage() {
             </Paper>
 
             {/* Title Input */}
-            <label htmlFor="title">
-              <Typography>Title of artwork</Typography>
-            </label>
-            <Input type="text" onChange={handleTextChange} id="title" />
+            <TextField
+              label="Title of Artwork"
+              value={title}
+              onChange={handleTextChange}
+              fullWidth
+              sx={{ mb: 1 }}
+              variant="outlined"
+            />
+
+            {/* Price Input */}
+            <NumericFormat
+              sx={{ mb: 1 }}
+              label="Art Price (USD)"
+              customInput={TextField}
+              type="text"
+              fullWidth
+              thousandSeparator
+              prefix="$"
+              onChange={handlePriceChange}
+              value={price}
+            />
+
+            {/* Tags Input */}
+            <Autocomplete
+              multiple
+              id="tags"
+              options={artisticStyles}
+              freeSolo
+              value={tags}
+              onChange={(event, newValue) => {
+                setTags(newValue);
+              }}
+              renderTags={(value, getTagProps) =>
+                value.map((option, index) => (
+                  <Chip
+                    variant="outlined"
+                    label={option}
+                    {...getTagProps({ index })}
+                  />
+                ))
+              }
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  variant="outlined"
+                  label="Tags"
+                  placeholder="Add Tags"
+                />
+              )}
+            />
 
             {/* Submit Button */}
             <Button
